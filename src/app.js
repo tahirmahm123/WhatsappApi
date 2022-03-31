b3ffa6f826908993d3a33d13034ff6de19803d13
Tahir Mahmood <30719791+tahirmahm123@users.noreply.github.com>
2022-03-31 12:59:22 +0500

    Fix

--- src/app.js
+++ src/app.js
@@ -16,49 +16,16 @@ create(
         (base64Qr, asciiQR, attempts, urlCode) => {
             console.log('Number of attempts to read the qrcode: ', attempts);
             console.log('Terminal qrcode: ', asciiQR);
-            console.log('base64 image string qrcode: ', base64Qrimg);
-            console.log('urlCode (data-ref): ', urlCode);
-            console.log(asciiQR); // Optional to log the QR in the terminal
-            const matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-                response = {};
-
-            if (matches.length !== 3) {
-                return new Error('Invalid input string');
-            }
-            response.type = matches[1];
-            response.data = new Buffer.from(matches[2], 'base64');
-
-            require('fs').writeFile(
-                'out.png',
-                response['data'],
-                'binary',
-                function (err) {
-                    if (err != null) {
-                        console.log(err);
-                    }
-                }
-            );
+            // console.log('base64 image string qrcode: ', base64Qr);
+            // console.log('urlCode (data-ref): ', urlCode);
+            // console.log(asciiQR); // Optional to log the QR in the terminal
         },
         undefined, // statusFind
         {
-            folderNameToken: "tokens", // folder name when saving tokens
-            mkdirFolderToken: "", // folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
-            headless: true, // Headless chrome
-            devtools: false, // Open devtools by default
-            useChrome: true, // If false will use Chromium instance
-            debug: false, // Opens a debug session
-            logQR: true, // Logs QR automatically in terminal
-            browserWS: "", // If u want to use browserWSEndpoint
-            browserArgs: ["--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"], // Original parameters  ---Parameters to be added into the chrome browser instance
-            puppeteerOptions: {}, // Will be passed to puppeteer.launch
-            disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
-            disableWelcome: true, // Will disable the welcoming message which appears in the beginning
-            updatesLog: true, // Logs info updates automatically in terminal
-            autoClose: false, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
-            createPathFileToken: false, // creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
-            addProxy: [""], // Add proxy server exemple : [e1.p.webshare.io:01, e1.p.webshare.io:01]
-            userProxy: "", // Proxy login username
-            userPass: "", // Proxy password,
+            headless: false,
+            browserArgs: ["--user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'"], // Original parameters  ---Parameters to be added into the chrome browser instance
+            multidevice: true,
+            useChrome: false,
             chromiumVersion: '818858',
         }, // options
         undefined, // BrowserSessionToken
